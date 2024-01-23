6595e1109449bc0e47f7afb99e1267328a5b49b7
Pevth <236639@edu.p.lodz.pl>
2024-01-23 08:08:33 +0100

    fix theme & add speech to text

--- app/src/main/java/com/example/jplantapp/FlowerActivity.java
+++ app/src/main/java/com/example/jplantapp/FlowerActivity.java
@@ -2,6 +2,8 @@ package com.example.jplantapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import java.text.DecimalFormat;
+import java.util.Locale;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -11,6 +13,8 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
 
 import com.bumptech.glide.Glide;
 import com.example.jplantapp.Response.FlowerResponse;
@@ -28,6 +32,8 @@ public class FlowerActivity extends BasicActivity {
     private ImageView photoImage;
     private FlowerResponse selectedFlower;
     private SharedPreferences userPreferences;
+    private TextToSpeech textToSpeech;
+    private TextView textView;
 
     private ImageButton backButton;
 
@@ -82,5 +88,92 @@ public class FlowerActivity extends BasicActivity {
                 onBackPressed();
             }
         });
+
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int langResult = textToSpeech.setLanguage(Locale.getDefault());
+
+                    if (langResult == TextToSpeech.LANG_MISSING_DATA || langResult == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    }
+                }
+            }
+        });
+        textToSpeech.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+            @Override
+            public void onStart(String utteranceId) {
+            }
+
+            @Override
+            public void onDone(String utteranceId) {
+            }
+
+            @Override
+            public void onError(String utteranceId) {
+            }
+        });
+
+        flowerNameTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speakText(flowerNameTextView.getText().toString());
+            }
+        });
+
+        accuracyTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speakText(accuracyTextView.getText().toString());
+            }
+        });
+
+        floweringTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speakText(floweringTextView.getText().toString());
+            }
+        });
+
+        growingTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speakText(growingTextView.getText().toString());
+            }
+        });
+
+        usageTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speakText(usageTextView.getText().toString());
+            }
+        });
+
+        notesTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speakText(notesTextView.getText().toString());
+            }
+        });
+
+        winterizingTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speakText(winterizingTextView.getText().toString());
+            }
+        });
+    }
+
+    private void speakText(String text) {
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onDestroy();
     }
 }
